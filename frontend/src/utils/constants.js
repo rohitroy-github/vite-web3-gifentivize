078b06c1b9dc9678f75cb92b199c04a677e20ba3
rohit <rhtry.tech@gmail.com>
2023-05-15 00:17:14 +0530

    add : frontend files

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,12 @@
+// ABI fetching?
+
+// whenFetchingFromCopiedABIFile
+import abi from "./jsonABI.json";
+
+// whenFetchingDirectlyFromArtifacts
+// import abi from "../../../backend (solidity-hardhat)/artifacts/contracts/Transactions.sol/Transactions.json";
+
+export const contractABI = abi.abi;
+
+// deployedTransactionAddress(SEPOLIA)
+export const contractAddress = "0x00025B5788B8781df5e632e3E49483c61839B93b";
