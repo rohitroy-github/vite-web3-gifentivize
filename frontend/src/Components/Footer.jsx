f717e9ac6a8c8f99824e3d82f2826989125370be
rohit <rhtry.tech@gmail.com>
2023-05-15 09:45:19 +0530

    update :

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -2,57 +2,70 @@
 
 import React from "react";
 
-const Footer = () => (
-  <div className="w-full flex justify-center items-center flex-col p-4 gradient-bg-footer">
-    <div className="md:w-[75%] w-full flex justify-center my-4">
-      <div className="md:flex-[1] justify-self-center tems-center">
-        <h3 className="text-2xl sm:text-3xl text-white text-gradient">
-          Gifentivize
-        </h3>
+const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div className="w-full flex justify-center items-center flex-col p-4 gradient-bg-footer">
+      <div className="md:w-[75%] w-full flex justify-center my-4">
+        <div className="md:flex-[1] justify-self-center tems-center">
+          <h3 className="text-2xl sm:text-3xl text-white text-gradient">
+            Gifentivize
+          </h3>
+        </div>
+        <div className="flex flex-1 justify-evenly items-center flex-wrap w-full mt-5 md:mt-2">
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            <a href="https://coinmarketcap.com/">Market</a>
+          </p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            <a href="https://wazirx.com/invite/vpxbjhfy">Exchange</a>
+          </p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            <a href="https://www.binance.com/en-IN/activity/referral-entry/CPA?fromActivityPage=true&ref=CPA_00SPT3IAOM">
+              Tutorials
+            </a>
+          </p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            <a href="https://github.com/rohitroy-github/react-ModernCryptoTransact">
+              Wallets
+            </a>
+          </p>
+          <button onClick={scrollToTop}>
+            <p className="text-white text-base text-center mx-2 cursor-pointer">
+              Scroll to top
+            </p>
+          </button>
+        </div>
       </div>
-      <div className="flex flex-1 justify-evenly items-center flex-wrap w-full mt-5 md:mt-2">
-        <p className="text-white text-base text-center mx-2 cursor-pointer">
-          <a href="https://coinmarketcap.com/">CoinMarketCap</a>
+
+      <div className="flex justify-center items-center flex-col mt-5">
+        <p className="text-white text-sm text-center">
+          Come join us in this new WEB3 journey !
         </p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">
-          <a href="https://wazirx.com/invite/vpxbjhfy">WazirX</a>
+        <p className="text-white text-sm text-center font-medium mt-2">
+          {" "}
+          {/* rht.tech@gmail.com */}
+          React | Blockchain | Web3 | MetaMask | HardHat
         </p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">
-          <a href="https://www.binance.com/en-IN/activity/referral-entry/CPA?fromActivityPage=true&ref=CPA_00SPT3IAOM">
-            Binance
+      </div>
+
+      <div className="sm:w-[75%] w-full h-[0.25px] bg-gray-400 mt-5 " />
+
+      <div className="sm:w-[75%] w-full flex justify-between items-center mt-3">
+        <p className="text-white text-left text-xs">Rohit Roy</p>
+        <p className="text-white text-right text-xs">
+          <a href="https://github.com/rohitroy-github/vite-web3-gifentivize">
+            Project | 2022
           </a>
         </p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">
-          <a href="https://github.com/rohitroy-github/react-ModernCryptoTransact">
-            Github
-          </a>
-        </p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">
-          <a href="https://metamask.io/">MetaMask</a>
-        </p>{" "}
       </div>
     </div>
-
-    <div className="flex justify-center items-center flex-col mt-5">
-      <p className="text-white text-sm text-center">
-        Come join us in this new WEB3 journey !
-      </p>
-      <p className="text-white text-sm text-center font-medium mt-2">
-        {" "}
-        {/* rht.tech@gmail.com */}
-        React | Blockchain | Web3 | MetaMask | HardHat
-      </p>
-    </div>
-
-    <div className="sm:w-[75%] w-full h-[0.25px] bg-gray-400 mt-5 " />
-
-    <div className="sm:w-[75%] w-full flex justify-between items-center mt-3">
-      <p className="text-white text-left text-xs">Rohit Roy</p>
-      <p className="text-white text-right text-xs">
-        All rights reserved | 2022
-      </p>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Footer;
--- frontend/src/Components/Welcome.jsx
+++ frontend/src/Components/Welcome.jsx
@@ -53,11 +53,11 @@ const Welcome = () => {
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
         <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
-            Send Crypto <br /> across the world
+            Send crypto <br /> across the world
           </h1>
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
             Explore the crypto world. Buy and sell cryptocurrencies easily on
-            Krypto.
+            Gifentivize.
           </p>
 
           {!currentAccount ? (
@@ -117,7 +117,7 @@ const Welcome = () => {
                   {shortenAddress(currentAccount)}
                 </p>
                 <p className="text-white font-semibold text-lg mt-1">
-                  Ethereum
+                  Ethereum Wallet
                 </p>
               </div>
             </div>
